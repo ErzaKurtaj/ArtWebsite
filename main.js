117f472ef0116d6f8e3a34c8cc558e229b0beb12
ErzaKurtaj <erzzaakurtaj@gmail.com>
2025-02-24 22:32:25 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,88 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const clickableHeadings = document.querySelectorAll('.clickable-heading');
+
+  clickableHeadings.forEach(heading => {
+      heading.addEventListener('click', function () {
+          const description = this.nextElementSibling;
+          if (description.style.display === 'none') {
+              description.style.display = 'block';
+          } else {
+              description.style.display = 'none';
+          }
+      });
+  });
+});
+
+// Modal functionality
+function viewMore(id) {
+    document.getElementById(id).style.display = 'flex';
+}
+
+function closeModal(id) {
+    document.getElementById(id).style.display = 'none';
+}
+
+// Close the modal when clicking outside of the modal content
+window.onclick = function(event) {
+    const modals = document.getElementsByClassName('modal');
+    for (let i = 0; i < modals.length; i++) {
+        if (event.target == modals[i]) {
+            modals[i].style.display = 'none';
+        }
+    }
+}
+
+// Canvas drawing functionality
+const canvas = document.getElementById('artCanvas');
+const context = canvas.getContext('2d');
+let drawing = false;
+let currentTool = 'brush';
+
+canvas.addEventListener('mousedown', startDrawing);
+canvas.addEventListener('mouseup', stopDrawing);
+canvas.addEventListener('mousemove', draw);
+
+function startDrawing(event) {
+    drawing = true;
+    draw(event); 
+}
+
+function stopDrawing() {
+    drawing = false;
+    context.beginPath(); 
+}
+
+function draw(event) {
+    if (!drawing) return;
+
+    context.lineWidth = 5;
+    context.lineCap = 'round';
+
+    const rect = canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    if (currentTool === 'brush') {
+        context.strokeStyle = document.getElementById('colorPicker').value;
+    } else if (currentTool === 'eraser') {
+        context.strokeStyle = '#FFFFFF';
+    }
+
+    context.lineTo(x, y);
+    context.stroke();
+    context.beginPath();
+    context.moveTo(x, y);
+}
+
+function clearCanvas() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function setTool(tool) {
+    currentTool = tool;
+}
+
+
+
+
+
